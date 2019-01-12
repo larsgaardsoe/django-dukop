0875aaff6c7ff71f0779f5d78cfc46b5ba909433
Benjamin Bach <benjamin@overtag.dk>
2019-01-12 20:41:27 +0100

    Refactor to dukop.settings instead of dukop.conf and use settings.local

--- src/dukop/urls.py
+++ src/dukop/urls.py
@@ -20,7 +20,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
 
 if settings.DEBUG:
     import debug_toolbar
--- src/dukop/wsgi.py
+++ src/dukop/wsgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dukop.conf.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dukop.settings")
 
 application = get_wsgi_application()
